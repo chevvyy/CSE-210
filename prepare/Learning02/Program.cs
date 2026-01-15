89a56db65576aa95ccc8f685ebc28a74fb3e0870
chevvyy <howdev02@gmail.com>
2026-01-14 21:27:31 -0700

    Learning03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job();
+        job1._jobTitle = "Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2017;
+        job1._endYear = 2021;
+
+        var job2 = new Job();
+        job2._jobTitle = "Team Lead";
+        job2._company = "Google";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        var myResume = new Resume();
+        myResume._name = "Devin";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
