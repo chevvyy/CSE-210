f70a8d098262984f1f3f43f26fba45ecac0d1426
chevvyy <howdev02@gmail.com>
2026-01-28 18:35:46 -0700

    W04 Prepare

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Getter so derived classes can safely access the student name
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,66 +1,24 @@
 using System;
-using System.Runtime.ExceptionServices;
-using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> _numbers = [];
-        int _number = 1;
+        // Test base class
+        Assignment a1 = new Assignment("Devin H", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
 
-        List<int> _sorted = [];
-        float _sum = 0;
+        // Test MathAssignment
+        MathAssignment m1 = new MathAssignment("Bryce T", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine();
 
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-
-        while (_number != 0)
-        {
-           Console.Write("Wnter number: ");
-           _number = int.Parse(Console.ReadLine());
-           if (_number != 0)
-           {
-                _numbers.Add(_number);
-           }
-            _sum += _number;
-        }
-
-        _sorted = _numbers;
-        _sorted.Sort();
-
-        float _average = 0;
-        int _total = 0;
-        int _largest = 0;
-        float _smallest = _sum;
-
-        foreach (int i in _sorted)
-        {
-            _total++;
-
-            if (i > _largest)
-            {
-                _largest = i;
-            }
-
-            if (i > 0 && i < _smallest)
-            {
-                _smallest = i;
-            }
-
-        }
-
-         _average = _sum/_total;
-
-         Console.WriteLine($"The Sum is: {_sum}");
-         Console.WriteLine($"The Average is: {_average}");
-         Console.WriteLine($"The Largest is: {_largest}");
-         Console.WriteLine($"The smallest positive is: {_smallest}");
-         Console.WriteLine($"The sorted list is: ");
-
-         foreach (int i in _sorted)
-        {
-            Console.Write($"{i}, ");
-        }
-
+        // Test WritingAssignment
+        WritingAssignment w1 = new WritingAssignment("Gabe H", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+        Console.WriteLine();
     }
 }
